4a5c163b1201ecac0c6f4401b5c39cfc8753b82d
Luiz Americo <blikblum@users.noreply.github.com>
2026-03-27 11:15:49 -0300

    Update page initialization

--- entrypoints/popup/testPlaywright.ts
+++ entrypoints/popup/testPlaywright.ts
@@ -1,13 +1,12 @@
-import { chromium } from 'playwright-crx'
+import { crx } from 'playwright-crx'
+
 
 export async function testPlaywright() {
-  const browser = await chromium.launch({
-    headless: false,
-  })
-  const page = await browser.newPage()
-  await page.goto('https://www.example.com')
+  console.log('Testing Playwright...')
+  const crxApp = await crx.start({ slowMo: 500 });
+  
+  const page = await crxApp.newPage()
+  await page.goto('https://example.com');
 
-  console.log(await page.title()) // Should print "Example Domain"
-
-  await browser.close()
+  console.log(await page.title()) // Should print "Example Domain"  
 }
